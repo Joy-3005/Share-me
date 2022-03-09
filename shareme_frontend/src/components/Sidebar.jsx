bc8c858683752ab9821bab651bac9a4f4bb4ce6a
Joy3005 <jyotirmoykarmakar02@gmail.com>
2022-03-09 08:38:33 +0530

    completed sidebar

--- shareme_frontend/src/components/Sidebar.jsx
+++ shareme_frontend/src/components/Sidebar.jsx
@@ -11,6 +11,27 @@ const isNotActiveStyle =
 const isActiveStyle =
   "flex items-center px-5 gap-3 font-extrabold border-r-2 border-black transition-all duration-200 ease-in-out capitalize";
 
+const categories = [
+  {
+    name: "Animals",
+  },
+  {
+    name: "Wallpapers",
+  },
+  {
+    name: "Photography",
+  },
+  {
+    name: "Gaming",
+  },
+  {
+    name: "Coding",
+  },
+  {
+    name: "Other",
+  },
+];
+
 const Sidebar = ({ user, closeToggle }) => {
   const handleCloseSidebar = () => {
     if (closeToggle) closeToggle(false);
@@ -33,6 +54,7 @@ const Sidebar = ({ user, closeToggle }) => {
             className={({ isActive }) =>
               isActive ? isActiveStyle : isNotActiveStyle
             }
+            onClick={handleCloseSidebar}
           >
             <RiHomeFill />
             Home
@@ -41,8 +63,35 @@ const Sidebar = ({ user, closeToggle }) => {
           <h3 className="mt-2 px-5 text-base 2xl:test-xl">
             Discover categories
           </h3>
+          {categories.slice(0, categories.length - 1).map((category) => (
+            <NavLink
+              to={`/category/${category.name}`}
+              className={({ isActive }) =>
+                isActive ? isActiveStyle : isNotActiveStyle
+              }
+              onClick={handleCloseSidebar}
+              key={category.name}
+            >
+              {category.name}
+            </NavLink>
+          ))}
         </div>
       </div>
+
+      {user && (
+        <Link
+          to={`user-profile/${user._id}`}
+          className="flex my-5 mb-3 gap-2 p-2 items-center bg-white rounded-lg shadow-lg mx-3"
+          onClick={handleCloseSidebar}
+        >
+          <img
+            src={user.image}
+            className="w-10 h-10 rounded-full"
+            alt="profile pic"
+          />
+          <p>{user.userName}</p>
+        </Link>
+      )}
     </div>
   );
 };

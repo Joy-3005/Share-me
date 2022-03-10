10f3855c0a1b97aa6927b82749010b5c55f3dc8f
LucidJoy <jyotirmoykarmakar02@gmail.com>
2022-03-10 11:37:18 +0530

    implemented pin component and pin functionalities

--- shareme_frontend/src/components/MasonryLayout.jsx
+++ shareme_frontend/src/components/MasonryLayout.jsx
@@ -15,7 +15,7 @@ const breakpointObj = {
 
 const MasonryLayout = ({ pins }) => {
   return (
-    <div className="flex animate-slide-fwd" breakpointCols={breakpointObj}>
+    <div className="flex animate-slide-fwd" breakpointcols={breakpointObj}>
       {pins?.map((pin) => (
         <Pin key={pin._id} pin={pin} className="w-max" />
       ))}

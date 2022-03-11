37153061a5ee5d87bb94175d47a1e6b27293b173
LucidJoy <jyotirmoykarmakar02@gmail.com>
2022-03-11 21:25:46 +0530

    user profile component done, search function implemented, categories generated dynamically

--- shareme_frontend/src/components/Sidebar.jsx
+++ shareme_frontend/src/components/Sidebar.jsx
@@ -3,6 +3,7 @@ import { NavLink, Link } from "react-router-dom";
 import { RiHomeFill } from "react-icons/ri";
 import { IoIosArrowForward } from "react-icons/io";
 
+import { categories } from "../utils/data";
 import logo from "../assets/logo.png";
 
 const isNotActiveStyle =
@@ -10,27 +11,6 @@ const isNotActiveStyle =
 
 const isActiveStyle =
   "flex items-center px-5 gap-3 font-extrabold border-r-2 border-black transition-all duration-200 ease-in-out capitalize";
-
-const categories = [
-  {
-    name: "Animals",
-  },
-  {
-    name: "Wallpapers",
-  },
-  {
-    name: "Photography",
-  },
-  {
-    name: "Gaming",
-  },
-  {
-    name: "Coding",
-  },
-  {
-    name: "Other",
-  },
-];
 
 const Sidebar = ({ user, closeToggle }) => {
   const handleCloseSidebar = () => {
@@ -72,6 +52,11 @@ const Sidebar = ({ user, closeToggle }) => {
               onClick={handleCloseSidebar}
               key={category.name}
             >
+              <img
+                src={category.image}
+                alt="category"
+                className="w-8 h-8 rounded-full shadow-sm"
+              />
               {category.name}
             </NavLink>
           ))}

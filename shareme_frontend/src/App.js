cd7f9b04ed0296a700d0357265e3a66929995594
LucidJoy <jyotirmoykarmakar02@gmail.com>
2022-03-11 21:51:24 +0530

    fixed googleId issue and bugs

--- shareme_frontend/src/App.js
+++ shareme_frontend/src/App.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Routes, Route, useNavigate } from "react-router-dom";
 
 import Login from "./components/Login";
 import Home from "./container/Home";
 
 const App = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const user = localStorage?.getItem("user");
+
+    if (!user) navigate("/login");
+  }, []);
+
   return (
     <Routes>
       <Route path="login" element={<Login />} />
